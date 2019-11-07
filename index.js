bcb4db3f2862cd698601b48dd2bdadef1ae6f576
MehalaKeller <55070236+MehalaKeller@users.noreply.github.com>
2019-11-07 14:16:05 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+const chalk = require('chalk');
+
+console.log(chalk.blue('CINDERELLA WALKED ON BROKEN GLASS'));
+console.log(chalk.gray('AURORA LET A WHOLE LIFETIME PASS'));
+console.log(chalk.yellow('BELLE FELL IN LOVE WITH A HIDEOUS BEAST'));
+console.log(chalk.blue('JASMINE MARRIED A COMMON THIEF'));
+console.log(chalk.gray('ARIEL WALKED ON LAND FOR LOVE AND LIFE'));
+console.log(chalk.red('SNOW WHITE BARELY ESCAPED THE KNIFE'));
+console.log(chalk.yellow('RAPUNZEL HAD TO FIND A NEW DREAM'));
+console.log(chalk.green('TIANA KISSED HER PRINCE AND TURNED GREEN'));
+console.log(chalk.red('MULAN LEFT TO BE A MAN'));
+console.log(chalk.yellow('POCAHONTAS STAYED TO SAVE HER LAND'));
+console.log(chalk.gray("IT'S ALL ABOUT THE SMILES AND TEARS;"));
+console.log(chalk.gray('BECAUSE LOVE MEANS FACING YOUR BIGGEST'));
+console.log(chalk.yellow('FEARS...'));
